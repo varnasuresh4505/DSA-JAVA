a9b878e777cc12a0aa6ee0ddd648410c3a239f86
varnasuresh4505 <varnasuresh4505@gmail.com>
2025-08-12 14:41:51 +0530

    linear search

---
+++ searchingAlgorithms/linearSearch.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class linearSearch 
+{
+    public static void main(String[] args)
+    {
+         Scanner sc=new Scanner(System.in);
+         int size=sc.nextInt();
+         int[] arr=new int[size];
+         for(int i=0;i<size;i++) 
+         {
+            arr[i]=sc.nextInt();
+         }
+         int target=sc.nextInt();
+         System.out.print(linearsearch(arr,target));
+    }
+    public static int linearsearch(int[] arr,int target)
+    {
+        for(int i=0;i<arr.length;i++) 
+        {
+            if(arr[i]==target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
