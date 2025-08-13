4fd912c4853b72b186e854a2c0447b7bb920383e
varnasuresh4505 <varnasuresh4505@gmail.com>
2025-08-13 22:33:40 +0530

    small change

--- searchingAlgorithms/binarySearch.java
+++ searchingAlgorithms/binarySearch.java
@@ -18,7 +18,7 @@ public class binarySearch
         int left=0,right=arr.length-1;
         while(left<=right)
         {
-            int mid=left+(right-left)/2;
+            int mid=left+(right-left)/2; 
             if(arr[mid]==target)
             {
                 return mid;
